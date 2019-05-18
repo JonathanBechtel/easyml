1353a3f84cbdb7b8ac3d517ee434fc3409681a41
Jonathan Bechtel <jonathanbechtel@gmail.com>
2019-05-17 21:42:44 -0400

    added better notes for Lasso, updated notebook

--- algorithms/LinearModels/lasso_regression.py
+++ algorithms/LinearModels/lasso_regression.py
@@ -1,16 +1,20 @@
-# -*- coding: utf-8 -*-
 """
 Lasso Regression Implemented with Coordinate Descent, written in Numpy
 """
 import numpy as np
 
 class LassoRegression():
-    
+    """
+    Takes the following variables at initialization:
+    n_iter: number of iterations to use for gradient descent
+    alpha:  value of alpha to use for regularization
+    """
     def __init__(self, n_iter=500, alpha=1):
         self.n_iter = n_iter
         self.alpha  = alpha
         
     def compute_rho(self, X, y, col_idx):
+        """sub derivative of variable"""
         pred   = self.predict(X)
         errors = y - pred
         rho    = X[:, col_idx] @ (errors + self.coef_[col_idx]*X[:, col_idx])
@@ -20,9 +24,11 @@ class LassoRegression():
         return X @ self.coef_
     
     def compute_zeta(self, X, col_idx):
+        """Value of zeta, used in the coordinate descent update"""
         return  1 / np.sum(X[:, col_idx]**2)
     
     def soft_threshold(self, rho, alpha):
+        """Soft threshold function to determine value of coefficient"""
         if rho < -alpha:
             return rho + alpha
         elif rho >= -alpha and rho <= alpha:
@@ -31,6 +37,7 @@ class LassoRegression():
             return rho - alpha
         
     def fit(self, X, y):
+        """update weights through coordinate descent"""
         self.coef_  = np.random.normal(loc=0.0, scale=0.1, size=X.shape[1])
         
         for _ in range(self.n_iter):
