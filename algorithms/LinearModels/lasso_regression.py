83b9dfb68ae5d8d05121096a4b76c9a8233cdbbd
Jonathan Bechtel <jonathanbechtel@gmail.com>
2019-05-31 22:30:06 -0400

    modified how X is standardized

--- algorithms/LinearModels/lasso_regression.py
+++ algorithms/LinearModels/lasso_regression.py
@@ -10,10 +10,10 @@ class LassoRegression():
     n_iter: number of iterations to use for gradient descent
     alpha:  value of alpha to use for regularization
     """
-    def __init__(self, n_iter=500, alpha=1, standardize=True):
-        self.n_iter      = n_iter
-        self.alpha       = alpha
-        self.standardize = standardize
+    def __init__(self, n_iter=500, alpha=1, centered=True):
+        self.n_iter   = n_iter
+        self.alpha    = alpha
+        self.centered = centered
         
     def compute_rho(self, X, y, col_idx):
         """sub derivative of variable"""
@@ -40,13 +40,16 @@ class LassoRegression():
         
     def fit(self, X, y):
         
-        if self.standardize:
-            X = standardize(X)
-        """update weights through coordinate descent"""
-        self.coef_  = np.random.normal(loc=0.0, scale=0.1, size=X.shape[1])
+        if self.centered:
+            X_pred = standardize(X)
+        else:
+            X_pred = X
+            
+        # update weights through coordinate descent
+        self.coef_  = np.random.normal(loc=0.0, scale=0.1, size=X_pred.shape[1])
         
         for _ in range(self.n_iter):
             for col in range(len(self.coef_)):
-                rho  = self.compute_rho(X, y, col)
-                zeta = self.compute_zeta(X, col) 
+                rho  = self.compute_rho(X_pred, y, col)
+                zeta = self.compute_zeta(X_pred, col) 
                 self.coef_[col] = zeta * self.soft_threshold(rho, self.alpha)
