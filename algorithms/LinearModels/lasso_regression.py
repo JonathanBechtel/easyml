e8aed2738983fb11ed6267c742328893acc81141
Jonathan Bechtel <jonathanbechtel@gmail.com>
2019-06-01 19:30:49 -0400

    updated comments, changed update rule for intercept

--- algorithms/LinearModels/lasso_regression.py
+++ algorithms/LinearModels/lasso_regression.py
@@ -6,30 +6,46 @@ from utils import standardize
 
 class LassoRegression():
     """
-    Takes the following variables at initialization:
-    n_iter: number of iterations to use for gradient descent
-    alpha:  value of alpha to use for regularization
+    Initializes LassoRegression Algorithm
+    Inputs:
+        
+    n_iter: integer
+    ---------------------
+    Number of iterations to use when implementing coordinate descent
+    
+    alpha: inter
+    ---------------------
+    Strength of tuning parameter alpha to be used in regularization
+    
+    centered: bool
+    ---------------------
+    Whether or not to standardize your data when calling fit()
     """
     def __init__(self, n_iter=500, alpha=1, centered=True):
         self.n_iter   = n_iter
         self.alpha    = alpha
         self.centered = centered
         
-    def compute_rho(self, X, y, col_idx):
-        """sub derivative of variable"""
+    def _compute_rho(self, X, y, col_idx):
+        """Subdifferential of variable"""
+        
+        # generate linear output from coefficients
         pred   = self.predict(X)
+        # get gradient, based off of MSE
         errors = y - pred
+        # take subdifferential of this particular column
         rho    = X[:, col_idx] @ (errors + self.coef_[col_idx]*X[:, col_idx])
         return rho
     
     def predict(self, X):
+        """Linear output based off of X and derived coefficients"""
         return X @ self.coef_
     
-    def compute_zeta(self, X, col_idx):
+    def _compute_zeta(self, X, col_idx):
         """Value of zeta, used in the coordinate descent update"""
         return  1 / np.sum(X[:, col_idx]**2)
     
-    def soft_threshold(self, rho, alpha):
+    def _soft_threshold(self, rho, alpha):
         """Soft threshold function to determine value of coefficient"""
         if rho < -alpha:
             return rho + alpha
@@ -40,16 +56,27 @@ class LassoRegression():
         
     def fit(self, X, y):
         
+        # standardize data, if specified
+        X_fit = np.zeros(X.shape)
         if self.centered:
-            X_pred = standardize(X)
+            X_fit = standardize(X)
         else:
-            X_pred = X
+            X_fit = X
+        # insert column of ones for intercept    
+        X_fit = np.insert(X_fit, 0, 1, axis=1)
             
-        # update weights through coordinate descent
-        self.coef_  = np.random.normal(loc=0.0, scale=0.1, size=X_pred.shape[1])
+        # initialize weights for each variable
+        self.coef_ = np.random.normal(loc=0.0, scale=0.1, size=X_fit.shape[1])
         
+        # initialize coordinate descent
         for _ in range(self.n_iter):
-            for col in range(len(self.coef_)):
-                rho  = self.compute_rho(X_pred, y, col)
-                zeta = self.compute_zeta(X_pred, col) 
+            # for every column except intercept
+            for col in range(1, len(self.coef_)):
+                # take its subdifferential
+                rho  = self.compute_rho(X_fit, y, col)
+                # calculate zeta
+                zeta = self.compute_zeta(X_fit, col)
+                # update weight depending on value of subdifferential and alpha
                 self.coef_[col] = zeta * self.soft_threshold(rho, self.alpha)
+        # after you've updated all weights, set the intercept -- should be y.mean() if data is centered        
+        self.coef_[0] = y.mean() - X_fit[:, 1:].mean(0) @ self.coef_[1:].T
